03f76f221b0b061641cbc856976561f9a4157eac
Michael LaDouceur <michaelladouceur1@gmail.com>
2019-07-11 00:18:09 -0500

    Expanded eel; Worked on styling with bootstrap

--- web/script.js
+++ web/script.js
@@ -1,15 +1,23 @@
 function setVal(item) {
     items = $(item);
-    table = item.split('.').join('')
-    values = {}
+    table = item.split('.').join('');
+    values = {};
     for (i=0; i<items.length; i++) {
         console.log($(items[i]).val())
         console.log($(items[i]).attr('name'))
         values[$(items[i]).attr('name')] = ($(items[i]).val())
-    }
+    };
     eel.setValue(table, values);
 };
 
-function loadSelect(classSelect) {
-	
-}
+function loadTable(items, selector) {
+    $(selector).append('<tr>');
+    for (i=0; i<items.length; i++) {
+        $(selector).append(
+            '<td>' + items[i] + '</td>'
+        );
+    };
+    $(selector).append(
+            '<td><a href="#"><i class="material-icons">settings</i></a></td>' +
+        '</tr>');
+};
