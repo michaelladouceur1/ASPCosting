f9c9c032ed6780f6af060202091fe30e68086f2b
Michael LaDouceur <michaelladouceur1@gmail.com>
2019-07-11 16:06:20 -0500

    Eel Functionality

--- run.py
+++ run.py
@@ -8,32 +8,67 @@ import time
 
 eel.init('web')
 
+eel.init();
+
 @eel.expose
 def setValue(table, value):
-    insert(table, value)
+	insert(table, value)
 
 def connect(func):
-    def wrapper(*args, **kwargs):
-        engine = create_engine('sqlite:///test2.db', echo=False)
-        Session = sessionmaker(engine)
-        data = func(session=Session(), *args, **kwargs)
-        Session().close()
-        return data 
-    return wrapper
+	def wrapper(*args, **kwargs):
+		engine = create_engine('sqlite:///test2.db', echo=False)
+		Session = sessionmaker(engine)
+		data = func(session=Session(), *args, **kwargs)
+		Session().close()
+		return data 
+	return wrapper
 
 @connect
 def query(table, mode, session, order=None, **kwargs):
-    if mode.lower() == 'first':
-        data = session.query(table).filter_by(**kwargs).first()
-    elif mode.lower() == 'all':
-        data = session.query(table).filter_by(**kwargs).order_by(order).all()
-    return data
+	print(f'query: {table}')
+	if mode.lower() == 'first':
+		data = session.query(table).filter_by(**kwargs).first()
+	elif mode.lower() == 'all':
+		data = session.query(table).filter_by(**kwargs).order_by(order).all()
+	return data
 
 @connect
 def insert(table, values, session):
-    print(f'Inserting ({values}) into {table}')
-    data = getattr(Standards, table)(**values)
-    session.add(data)
-    session.commit()
+	print(f'Inserting ({values}) into {table}')
+	data = getattr(Standards, table)(**values)
+	session.add(data)
+	session.commit()
 
-eel.start('main.html', size=(800,500))
+def outputList(data, items):
+	print('output list')
+	list = []
+	for item in data:
+		for i in items:
+			# print(getattr(item, i))
+			list.append(getattr(item, i))
+	print(list)
+	return list
+
+@eel.expose
+def loadTable(table, items):
+	data = tables(table)
+	table = data[0]
+	mode = data[1]
+	order = data[2]
+	selector = data[3]
+	data = query(table, mode, order=order)
+	data = outputList(data, items)
+	print('calling loadTable()')
+	eel.loadTable(data, selector)
+
+def tables(name):
+	tables = {
+		'materialType': [Standards.MaterialType, 'all', Standards.MaterialType.name, '#materialTypeTable'],
+		'material': [Standards.Material, 'all', Standards.Material.name, '#materialTypeTable']
+	}
+	print(tables[name])
+	return tables[name]
+
+loadTable('materialType',['name'])
+# loadTable('material',['materialType','name','density'])
+eel.start('main.html', size=(1000,600))
--- web/script.js
+++ web/script.js
@@ -3,21 +3,40 @@ function setVal(item) {
     table = item.split('.').join('');
     values = {};
     for (i=0; i<items.length; i++) {
-        console.log($(items[i]).val())
-        console.log($(items[i]).attr('name'))
         values[$(items[i]).attr('name')] = ($(items[i]).val())
     };
     eel.setValue(table, values);
+    eel.loadTable('materialType', ['name'])
 };
 
+eel.expose(loadTable);
 function loadTable(items, selector) {
-    $(selector).append('<tr>');
-    for (i=0; i<items.length; i++) {
-        $(selector).append(
-            '<td>' + items[i] + '</td>'
-        );
-    };
-    $(selector).append(
-            '<td><a href="#"><i class="material-icons">settings</i></a></td>' +
-        '</tr>');
+	$(selector).empty()
+	index = 0;
+	items.forEach((item) => {
+		$(selector).append(
+    	'<tr>' +
+    		'<td>' + item + '</td>' +
+    		'<td>' +
+    			'<a href="#"><i class="material-icons">settings</i></a>' +
+    			'<button type="button" class="delete" aria-label="Close">' +
+					'<span aria-hidden="true">&times;</span>' +
+				'</button>' +
+    		'</td>' +
+    	'</tr>'
+    	);
+	});
+};
+
+function deleteTableItem(target) {
+	$(target).closest('tr').remove()
+}
+
+eel.expose(init);
+function init() {
+	$(document).ready(function() {
+		$(".delete").click(function(event) {
+			deleteTableItem(event.target);
+		});
+	});
 };
